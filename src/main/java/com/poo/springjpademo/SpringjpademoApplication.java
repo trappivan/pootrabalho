5232d88e7b9c6ae9eaf3da8ac9139c507d01500f
trappivan <ivan.trapp@univille.br>
2024-04-26 20:04:29 -0300

    finalizando projeto

--- src/main/java/com/poo/springjpademo/SpringjpademoApplication.java
+++ src/main/java/com/poo/springjpademo/SpringjpademoApplication.java
@@ -115,50 +115,6 @@ public class SpringjpademoApplication {
 			List<Disciplina> sistemasInfo = disciplinaRepository.findDisciplinaByCurso(sistemasInformacao);
 			sistemasInfo.forEach(System.out::println);
 
-
-			/*
-			repository.save(new Professor("Leanderson"));
-			repository.save(new Professor("Paulo"));
-			repository.save(new Professor("Vanessa"));
-			log.info("-------------------------------");
-			log.info(" findAll");
-			for(var p : repository.findAll()){
-				log.info(p.toString());
-			}
-			log.info("-------------------------------");
-			log.info(" findAllOrderByNomeDesc");
-			for(var p : repository.findAll(Sort.by(Sort.Direction.DESC,"nome"))){
-				log.info(p.toString());
-			}
-			log.info("-------------------------------");
-			log.info(" findById");
-			var p = repository.findById(1L);
-			log.info(p.toString());
-			log.info("-------------------------------");
-			log.info(" findByINome");
-			 p = repository.findByNome("Vanessa");
-			log.info(p.toString());
-			p = repository.findById(1L);
-			disciplinaRepository.save(new Disciplina("Poo 1", p.get()));
-			disciplinaRepository.save(new Disciplina("Poo 2", p.get()));
-			disciplinaRepository.save(new Disciplina("PCE", p.get()));
-
-			p = repository.findById(2L);
-			disciplinaRepository.save(new Disciplina("IA", p.get()));
-			disciplinaRepository.save(new Disciplina("Redes", p.get()));
-			log.info("-------------------------------");
-			log.info(" Disciplinas");
-			for(var d : disciplinaRepository.findAll()){
-				log.info(d.toString());
-			}
-			p = repository.findById(3L);
-			log.info("-------------------------------");
-			log.info(" Disciplinas do professor paulo");
-			for(var d : disciplinaRepository.findAllByProfessor(p.get())){
-				log.info(d.toString());
-			}
-
-			 */
 		};
 
 
